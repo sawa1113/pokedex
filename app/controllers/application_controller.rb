7bf2e6f2f2f07e42b3d9d26c5d772e8552716262
sawa1113 <63143529+sawa1113@users.noreply.github.com>
2020-11-22 21:01:37 +0900

    ユーザーのプロフィール画面作成と編集（デフォルトをカスタマイズ）

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,17 @@
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller?
+    protect_from_forgery with: :exception
 
+    # ログイン済ユーザーのみにアクセスを許可する
+    before_action :authenticate_user!
+
+    # deviseコントローラーにストロングパラメータを追加する          
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    
+    protected
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:name]) # 新規登録時(sign_up時)にnameというキーのパラメーターを追加で許可する
+      # サインアップ時にnameのストロングパラメータを追加
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+      # アカウント編集の時にnameとprofileのストロングパラメータを追加
+      devise_parameter_sanitizer.permit(:account_update, keys: [:name, :profile])
     end
 end
